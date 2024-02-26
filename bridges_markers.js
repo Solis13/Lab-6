0864da21773f165e8e08cf216f4500b98e214099
Solis13 <156252616+Solis13@users.noreply.github.com>
2024-02-25 23:14:24 -0600

    Lab6

---
+++ bridges_markers.js
@@ -0,0 +1,75 @@
+let mapCenter = [44, -103]
+// how much zoom in / out
+let zoomLevel = 2
+
+let bridgeMap = L.map('bridge-map').setView(mapCenter, zoomLevel)
+
+// Add the tile layer - roads, streets etc. Without this, nothing to see
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copywrite">OpenStreetMap</a>',
+}).addTo(bridgeMap);  // add the tileLayer to the bridge map
+
+let bridgeData = [    //span (meters)
+    {
+        name: 'Verrazano-Narrows Bridge',
+        cityState: 'New York, NY',
+        span:'1298.4',
+        coordinates: [40.6066, -74.0447]
+    },
+    {
+        name: 'Golden Gate Bridge',
+        cityState: 'San Francisco and Marin, CA',
+        span:'1280.2',
+        coordinates: [37.8199, -122.4783]
+    },
+    // make three more obj for other bridges
+
+    {
+        name: 'Mackinac Brigde',
+        cityState: 'Mackinaw and St Ignance, MI',
+        span: '1158.0',
+        coordinates: [45.8174, -84.7278]
+
+    },
+
+    {
+        name: 'George Washington Bridge',
+        cityState: '	New York, NY and New Jersey, NJ',
+        span:'1067.0',
+        coordinates: [	40.8517, -73.9527]
+    },
+
+    {
+        name: 'Tacoma Narrows Bridge',
+        cityState: 'Tacoma and Kitsap, WA',
+        span:'853.44',
+        coordinates: [	47.2690, -122.5517]
+    }
+
+]
+// insert an brigde incon here
+
+let bridgeIcon = L.icon({
+    iconUrl: 'brigde.png',
+
+    iconSize:     [38, 95],  
+    iconAnchor:   [22, 94], 
+    popupAnchor:  [-3, -76].
+
+   L.marker([51.5, -0.09], {icon: brigdeIcon}).addTo(map)
+
+})
+   let BridgeIcon = new BridgeIcon({iconUrl: 'bridge-green.png'}),
+   redIcon = new BridgeIcon({iconUrl: 'bridge-red.png'}),
+   orangeIcon = new BridgeIcon({iconUrl: 'bridge-orange.png'}),
+   yellowIcon = new BridgeIcon({iconUrl: 'bridge-yellow.png'}),
+   blackIcon = new BridgeIcon({iconUrl: 'bridge-black.png'})
+
+
+
+    bridgeData.forEach(function (bridge) {
+        let bridgeCoordinates = bridge.coordinates
+        let bridgeMarker = L.marker(bridgeCoordinates).addTo(bridgeMap)
+        // Create popup from data in bridge object
+        bridgeMarker.bindPopup`<b>${bridge.name}</b><br>${bridge.cityState}<br>Span: ${bridge.span} meters`
+      })
